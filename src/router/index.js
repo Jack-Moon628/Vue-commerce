4c8430c2f00e11e8e41544aad50e4d5db08c3d8d
unknown <bright_96_628@outlook.com>
2021-01-26 10:26:28 +0800

    login, signup page added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +7,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'signup',
+      component: () => import('../components/Signup.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../components/Login.vue')
+    },
+    {
+      path: '/forgot-password',
+      name: 'forgot-password',
+      component: () => import('../components/ForgotPassword.vue')
     }
   ]
 })
